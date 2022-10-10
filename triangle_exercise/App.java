677f8c645d2c43d67c4a94a43835ec8381d05aba
vortex2jm <joaopaulomoura5328@gmail.com>
2022-10-10 12:59:06 -0300

    :sparkles: Creating objects

--- triangle_exercise/App.java
+++ triangle_exercise/App.java
@@ -2,6 +2,12 @@ public class App {
   
   public static void main( String[] args ){
     
-    System.out.println(args[1]);
+    Point a = new Point(Double.parseDouble(args[0]), Double.parseDouble(args[1]));
+    Point b = new Point(Double.parseDouble(args[2]), Double.parseDouble(args[3]));
+    Point c = new Point(Double.parseDouble(args[4]), Double.parseDouble(args[5]));
+
+    Triangle tri = new Triangle(a,b,c); 
+    
+    System.out.println(tri.getPerimeter());
   }
 }
